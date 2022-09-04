9692eea0171993f5fe1c3ed575c44f57ff1cc45f
Alexandru Florea <alexflorea16000@gmail.com>
2022-09-04 20:12:24 +0300

    File download

--- src/main/java/com/example/app/FileResource.java
+++ src/main/java/com/example/app/FileResource.java
@@ -1,19 +1,19 @@
 package com.example.app;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.glassfish.jersey.media.multipart.BodyPart;
 import org.glassfish.jersey.media.multipart.BodyPartEntity;
 import org.glassfish.jersey.media.multipart.MultiPart;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/file")
 public class FileResource {
@@ -41,5 +41,24 @@ public class FileResource {
         }
 
         return Response.status(Response.Status.CREATED).entity(fileIds).build();
+    }
+
+    @GET
+    @Path("/{fileId}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response download(@PathParam("fileId") String fileId) {
+        Optional<File> fileOptional = storageService.getFromStorage(fileId);
+
+        if (fileOptional.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        File file = fileOptional.get();
+        String originalName = storageService.getOriginalFileName(file.getName());
+
+        return Response
+                .ok(file)
+                .header("Content-Disposition", "attachment; filename=\"" + originalName + "\"")
+                .build();
     }
 }
--- src/main/java/com/example/app/FileStorageService.java
+++ src/main/java/com/example/app/FileStorageService.java
@@ -6,16 +6,20 @@ import jakarta.enterprise.context.ApplicationScoped;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @ApplicationScoped
 public class FileStorageService {
 
     private final String STORAGE_PATH = "file_storage";
+    private File storageDirectory;
 
     @PostConstruct
     public void init() {
-        File storageDirectory = new File(STORAGE_PATH);
+        storageDirectory = new File(STORAGE_PATH);
         if (!storageDirectory.exists()){
             storageDirectory.mkdirs();
         }
@@ -33,5 +37,25 @@ public class FileStorageService {
         }
 
         return uuid.toString();
+    }
+
+    public Optional<File> getFromStorage(String uuid) {
+        Optional<File> fileOptional = Optional.empty();
+
+        for (File file : storageDirectory.listFiles()) {
+            String fileName = file.getName();
+            if (fileName.contains(uuid)) {
+                fileOptional = Optional.of(file);
+            }
+        }
+
+        return fileOptional;
+    }
+
+    public String getOriginalFileName(String storageName) {
+        List<String> splitName = Arrays.stream(storageName.split("\\.")).skip(1).toList();
+
+        return splitName.stream()
+                .reduce("", (prev, curr) -> prev + "." + curr);
     }
 }
